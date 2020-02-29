d586c33f18952ec3fb4963bc6a04af02741201ca
ETenal <etenal@foxmail.com>
2020-02-28 19:40:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from stock import Stock_bot
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print("Usage: ./Stock_bot TOKEN")
         exit(0)
 
