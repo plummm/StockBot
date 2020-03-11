c91c93dce6e03eb29fda608603c487071f56aef6
plummm <etenalcxz@gmail.com>
2020-03-10 20:25:45 -0700

    fix bugs, add time stamp

--- stock.py
+++ stock.py
@@ -5,8 +5,12 @@ import teleg_cmd
 from telegram import Bot
 from telegram.ext import Updater
 from telegram import InlineQueryResultArticle, InputTextMessageContent
+from datetime import datetime, timedelta
+from pytz import timezone
+
 import logging
 import time
+import threading
 
 class Stock_bot:
     def __init__(self, token):
@@ -15,6 +19,7 @@ class Stock_bot:
         self.initTelegram(token)
         self.initLogging()
         self.initCache()
+        self.initTime()
 
     def initTelegram(self, token):
         teleg_cmd.updater = Updater(token=token, use_context=True)
@@ -37,20 +42,42 @@ class Stock_bot:
     def initCache(self):
         ids = self.__getLocalChatId()
         for id in ids:
-            print(id)
             self.symCachePath[id] = "./sym-"+str(id)
             self.__getLocalSym(id)
-            print(teleg_cmd.gSym)
 
     def initLogging(self):
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                             level=logging.INFO)
 
+    def initTime(self):
+        nyc = timezone('America/New_York')
+        today = datetime.today().astimezone(nyc)
+        today_str = datetime.today().astimezone(nyc).strftime('%Y-%m-%d')
+        calendar = req_cmd.getMarketCalendar(today_str)
+        market_open = today.replace(
+            hour=calendar.open.hour,
+            minute=calendar.open.minute,
+            second=0
+        )
+        market_open = market_open.astimezone(nyc)
+        market_close = today.replace(
+            hour=calendar.close.hour,
+            minute=calendar.close.minute,
+            second=0
+        )
+        market_close = market_close.astimezone(nyc)
+
+        current_dt = datetime.today().astimezone(nyc)
+        self.__prepareWatcher(market_open, market_close)
+
+    #def watchPriceTrend(self, market_open, market_close, sym2ChatId):
+
     def showThePrice(self, update, sym):
         price = self.__getPrice(sym)
         if price != -1:
             teleg_cmd.sendMessages(update.effective_chat.id, "Current price of " + sym + " is $" + str(price))
-            teleg_cmd.gSym[update.effective_chat.id][sym]["currentPrice"] = price
+            if sym in teleg_cmd.gSym[update.effective_chat.id]:
+                teleg_cmd.gSym[update.effective_chat.id][sym]["currentPrice"] = price
 
     def Add2WatchList(self, chat_id, sym):
         print(teleg_cmd.gSym)
@@ -84,21 +111,21 @@ class Stock_bot:
         chat_id = update.effective_chat.id
         if not self.__isChatRegistered(chat_id):
             return
-        if teleg_cmd.userStatus[update.effective_user.id] == teleg_cmd.StatusAddToWatchList:
+        if teleg_cmd.userStatus[chat_id] == teleg_cmd.StatusAddToWatchList:
             if self.Add2WatchList(chat_id, update.message.text):
                 teleg_cmd.sendMessages(chat_id, update.message.text + " has been added to the watchlist.\n"
                                        + "The current price is $" + str(teleg_cmd.gSym[chat_id][update.message.text]["currentPrice"]))
             else:
                 teleg_cmd.sendMessages(chat_id, update.message.text + " is not a valid stock symbol.")
-        elif teleg_cmd.userStatus[update.effective_user.id] == teleg_cmd.StatusRemoveFromWatchList:
+        elif teleg_cmd.userStatus[chat_id] == teleg_cmd.StatusRemoveFromWatchList:
             if self.RemoveFromWatchList(chat_id, update.message.text):
                 teleg_cmd.sendMessages(chat_id, update.message.text + " has been removed from the watchlist.")
             else:
                 teleg_cmd.sendMessages(chat_id, "There is no " + update.message.text + " in the watchlist.")
-        elif teleg_cmd.userStatus[update.effective_user.id] == teleg_cmd.StatusGetPrice:
+        elif teleg_cmd.userStatus[chat_id] == teleg_cmd.StatusGetPrice:
             self.showThePrice(update, update.message.text)
 
-        teleg_cmd.userStatus[update.effective_user.id] = teleg_cmd.StatusNone
+        teleg_cmd.userStatus[chat_id] = teleg_cmd.StatusNone
 
     def CallbackStockPick(self, update, context):
         query = update.callback_query
@@ -147,3 +174,15 @@ class Stock_bot:
             teleg_cmd.sendMessages(chat_id, "/start first.")
             return False
         return True
+
+    def __prepareWatcher(self, market_open, market_close):
+        sym2ChatId = {}
+        for id in teleg_cmd.gSym:
+            for sym in teleg_cmd.gSym[id]:
+                if len(sym2ChatId[sym]) == 0:
+                    sym2ChatId[sym] = []
+                if id not in sym2ChatId[sym]:
+                    sym2ChatId[sym].append(id)
+        th = threading.Thread(target=self.watchPriceTrend(), args=(market_open, market_close, sym2ChatId))
+        th.start()
+
